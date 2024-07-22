501cb3311795a2cf5db5af8e39e0df826eedbeb6
repositoryVPS <vpstrader93@gmail.com>
2024-07-22 14:50:31 -0300

    adjs

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Security.Cryptography;
 using Unity.VisualScripting;
+using Unity.VisualScripting.ReorderableList.Element_Adder_Menu;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -11,26 +12,38 @@ public class PlayerController : MonoBehaviour
     [Header("Lerp")]
     public Transform target;
     public float lerpSpeed = 1f;
+    public string tagToCheckEnemy = "Enemy";
+
 
     private Vector3 _pos;
-    void Start()
-    {
+    private bool _canRun;
 
+    void Start()
+    { 
+        _canRun = true;
     }
 
 
     // Update is called once per frame
     void Update()
     {
+        if(!_canRun) return;
         _pos = target.position;
         _pos.y = transform.position.y;
         _pos.x = transform.position.x;
 
-
-        transform.position = Vector3.Lerp(transform.position, _pos, lerpSpeed * Time.deltaTime);
         transform.Translate(transform.forward * speed * Time.deltaTime);
+        transform.position = Vector3.Lerp(transform.position, _pos, lerpSpeed * Time.deltaTime); 
     }
-    
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == tagToCheckEnemy)
+        {
+            _canRun = false;
+        }
+    }
+
 }
 
-   
+
