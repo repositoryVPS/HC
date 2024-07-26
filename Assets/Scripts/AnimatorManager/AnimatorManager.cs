59063997244533e8e5ad4ef2b89f11eddeebe720
repositoryVPS <vpstrader93@gmail.com>
2024-07-26 12:55:36 -0300

    adj

--- Assets/Scripts/AnimatorManager/AnimatorManager.cs
+++ Assets/Scripts/AnimatorManager/AnimatorManager.cs
@@ -6,7 +6,30 @@ using UnityEngine;
 public class AnimatorManager : MonoBehaviour
 {
     public Animator animator;
+    public List<AnimatorSetup> animators;
 
+    public enum AnimationType 
+    {
+        RUN,
+        IDLE,
+        DEATH
+    }
 
-
+    public void Play(AnimationType type)
+    {
+        foreach (var animation in animators)
+        {
+            if(animation.type == type)
+            {
+                animator.SetTrigger(animation.trigger   );
+                break;
+            }
+        }
+    }
+}
+[System.Serializable]
+public class AnimatorSetup 
+{
+    public AnimatorManager.AnimationType type;
+    public string trigger;
 }
