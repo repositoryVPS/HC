bd866bf517d25e78112fd78a7e343509d18c2816
repositoryVPS <vpstrader93@gmail.com>
2024-07-31 16:12:04 -0300

    ads

--- Assets/Scripts/LevelManager/LevelManager.cs
+++ Assets/Scripts/LevelManager/LevelManager.cs
@@ -8,22 +8,29 @@ public class LevelManager : MonoBehaviour
 
     [SerializeField]
     public List<GameObject> levels;
+
+    [Header("Pieces")]
+    public List<LevelPieceBase> levelPieces;
+    public int piecesNumber = 5;
+
     private int _index;
 
     private GameObject _currentlevel;
+    private List<LevelPieceBase> _spawnedPieces;
 
     private void Awake()
     {
-        SpawnLevel();
+        //SpawnLevel();
+        CreateLevelPieces();
     }
     private void SpawnLevel()
     {
-        if(_currentlevel!= null)
+        if (_currentlevel != null)
         {
             Destroy(_currentlevel);
             _index++;
 
-            if(_index >= levels.Count)
+            if (_index >= levels.Count)
             {
                 ResetLevelIndex();
             }
@@ -36,4 +43,29 @@ public class LevelManager : MonoBehaviour
     {
         _index = 0;
     }
+
+    #region
+    private void CreateLevelPieces()
+    {
+        _spawnedPieces = new List<LevelPieceBase>();
+
+        for (int i = 0; i < piecesNumber; i++)
+        {
+            CreateLevelPiece();
+        }
+    }
+    private void CreateLevelPiece()
+    {
+        var piece = levelPieces[Random.Range(0, levelPieces.Count)];
+        var spawnedPiece = Instantiate(piece, container);
+
+
+        if (_spawnedPieces.Count > 0)
+        {
+            var lastPiece = _spawnedPieces[_spawnedPieces.Count - 1];
+            spawnedPiece.transform.position = lastPiece.endPiece.position;
+        }
+        _spawnedPieces.Add(spawnedPiece);
+    }
+    #endregion
 }
